fba6793e1cebbf884240411dbba3db4108725f1b
Manoj Mohan <m.manoj@linuxmail.org>
2016-10-10 14:57:46 +0530

    update

--- proj/settings.py
+++ proj/settings.py
@@ -235,11 +235,11 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 #EMAIL_USE_TLS = True
 #EMAIL_HOST = 'smtp.mail.com'
 #EMAIL_PORT = 587
-#EMAIL_HOST_USER = '***REMOVED***'
-#EMAIL_HOST_PASSWORD = '***REMOVED***'
+#EMAIL_HOST_USER = 
+#EMAIL_HOST_PASSWORD = 
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'trustlist1***REMOVED***'
-EMAIL_HOST_PASSWORD = 'Manoj@123'
+EMAIL_HOST_USER = 
+EMAIL_HOST_PASSWORD = 
